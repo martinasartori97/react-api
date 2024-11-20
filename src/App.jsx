02d70a4acb4a94bb265a87fd651cfafa6ef2e989
martinasartori97 <martinasartori9@gmail.com>
2024-11-20 17:56:32 +0100

    edit js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 
@@ -25,6 +25,47 @@ function App() {
 
 
 
+
+  function handleTitleOnChange(e) {
+    console.log(e);
+    setTitle(e.target.value)
+
+  }
+
+  function addArticle(e) {
+    e.preventDefault()
+    console.log(articles);
+
+
+
+  }
+
+  function handleButtonOnClick() {
+    console.log('mostrare gli articoli');
+
+
+  }
+
+  function handleImageOnChange(e) {
+    console.log(e);
+    setImage(e.target.value)
+
+  }
+
+  function handleContentOnChange(e) {
+    console.log(e);
+    setContent(e.target.value)
+
+  }
+
+  function handleCategoryOnChange(e) {
+    console.log(e);
+    setCategory(e.target.value)
+
+  }
+
+
+
   function fetchData(url = 'https://localhost:3000/posts') {
     fetch(url)
       .then(resp => resp.json())
@@ -34,115 +75,113 @@ function App() {
       })
 
 
+    useEffect(fetchData, [])
+  }
+}
+
+
+return (
+  <>
+    <div className="container">
+      <h1> React Blog Form</h1>
+      <p>insert the title</p>
+      <input type="text" title='title' id='title' value={title} onChange={e => setTitle(e.target.value)} />
+      <button onClick={handleButtonOnClick}>send</button>
+
+      <ul className="list-group">
+        {articles.map((article, index) => <li key={index} className="list-group-item">{article}</li>)}
+      </ul>
+
+
+      <div className="form-floating mb-3">
+
+        <input type="text" className="form-control" value={image} onChange={e => setImage(e.target.value)} />
+        <label className="img" htmlFor="floatingInputDisabled">insert the image</label>
+
+      </div>
+
+
+      <div className="form-floating mb-3">
+        <input type="text" className="form-control" value={content} onChange={e => setContent(e.target.value)} />
+        <label htmlFor="floatingInputDisabled">content</label>
+      </div>
+
+
+      <select className="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+
+
+      <div className="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        <label className="form-check-label" for="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+        <label className="form-check-label" for="flexCheckChecked">
+          Checked checkbox
+        </label>
+      </div>
+      <div className="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        <label className="form-check-label" for="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+        <label className="form-check-label" for="flexCheckChecked">
+          Checked checkbox
+        </label>
+      </div>
+    </div>
 
 
 
-    function handleTitleOnChange(e) {
-      console.log(e);
-      setTitle(e.target.value)
-
-    }
-
-    function addArticle(e) {
-      e.preventDefault()
-      console.log(articles);
+    <section className='posts'>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
 
 
+          {
+            postsData.results ?
 
-    }
+              postsData.results.map(posts => (
 
-    function handleButtonOnClick() {
-      console.log('mostrare gli articoli');
+                <div className="col" key={post.id}>
+                  <div className="card">
+                    <img src={post.image} alt="" />
+                    <p>
+                      {post.name}
+                    </p>
+                  </div>
+                </div>
 
+              )) :
 
-    }
+         
 
-    function handleImageOnChange(e) {
-      console.log(e);
-      setImage(e.target.value)
-
-    }
-
-    function handleContentOnChange(e) {
-      console.log(e);
-      setContent(e.target.value)
-
-    }
-
-    function handleCategoryOnChange(e) {
-      console.log(e);
-      setCategory(e.target.value)
-
-    }
-
-    return (
-      <>
-        <div className="container">
-          <h1> React Blog Form</h1>
-          <p>insert the title</p>
-          <input type="text" title='title' id='title' value={title} onChange={e => setTitle(e.target.value)} />
-          <button onClick={handleButtonOnClick}>send</button>
-
-          <ul className="list-group">
-            {articles.map((article, index) => <li key={index} className="list-group-item">{article}</li>)}
-          </ul>
-
-
-          <div className="form-floating mb-3">
-
-            <input type="text" className="form-control" value={image} onChange={e => setImage(e.target.value)} />
-            <label className="img" htmlFor="floatingInputDisabled">insert the image</label>
-
-          </div>
-
-
-          <div className="form-floating mb-3">
-            <input type="text" className="form-control" value={content} onChange={e => setContent(e.target.value)} />
-            <label htmlFor="floatingInputDisabled">content</label>
-          </div>
-
-
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-
-
-          <div className="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label className="form-check-label" for="flexCheckDefault">
-              Default checkbox
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
-            <label className="form-check-label" for="flexCheckChecked">
-              Checked checkbox
-            </label>
-          </div>
-          <div className="form-check">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
-            <label className="form-check-label" for="flexCheckDefault">
-              Default checkbox
-            </label>
-          </div>
-          <div className="form-check">
-            <input className="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
-            <label className="form-check-label" for="flexCheckChecked">
-              Checked checkbox
-            </label>
-          </div>
+      }
 
 
 
 
         </div>
+      </div>
 
-      </>
-    )
-  }
+    </section>
+
+
+  </>
+)
 }
+
+
+
 
 export default App
